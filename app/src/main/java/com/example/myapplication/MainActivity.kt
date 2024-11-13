2edb828a533e9feecb790a05b7688957f7beabe7
fagner ferreira <fagnerrc12@gmail.com>
2024-11-13 15:11:25 -0300

    profile activity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.myapplication
 
+import android.R.attr.value
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        findViewById<Button>(R.id.bt).setOnClickListener {
+            val myIntent: Intent = Intent(
+                this.baseContext,
+                ProfileActivity::class.java
+            )
+            myIntent.putExtra("key", value) //Optional parameters
+            this.startActivity(myIntent)
         }
     }
 }
