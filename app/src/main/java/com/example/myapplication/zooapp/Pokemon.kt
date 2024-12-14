c80641c077c5ba95a9bc3e17b0c74642dc585904
fagner ferreira <fagnerrc12@gmail.com>
2024-12-14 00:52:37 -0300

    pokemon app

--- app/src/main/java/com/example/myapplication/zooapp/Pokemon.kt
+++ app/src/main/java/com/example/myapplication/zooapp/Pokemon.kt
@@ -2,11 +2,7 @@
 
 package com.example.myapplication.zooapp
 
-import androidx.compose.animation.core.Transition
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.animateDp
-import androidx.compose.animation.core.updateTransition
+import android.graphics.Color.parseColor
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -16,7 +12,10 @@ import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.stopScroll
 import androidx.compose.foundation.layout.Arrangement
@@ -31,17 +30,21 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -52,50 +55,31 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import android.graphics.Color.parseColor
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.foundation.border
-import androidx.compose.foundation.interaction.InteractionSource
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextFieldDefaults
-import androidx.compose.material3.TextFieldColors
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.TextFieldDefaults.Container
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
-import androidx.media3.container.Mp4Box.ContainerBox
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -108,13 +92,12 @@ import com.example.myapplication.zooapp.ui.theme.typeColors
 import com.google.gson.Gson
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
-import io.ktor.client.request.url as _url
 import io.ktor.client.statement.bodyAsText
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlin.time.Duration.Companion.milliseconds
 import kotlinx.serialization.Serializable
-import okhttp3.internal.wait
+import kotlin.time.Duration.Companion.milliseconds
+import io.ktor.client.request.url as _url
 
 @Serializable
 data class ApiResource(val url: String, val name:String)
@@ -313,7 +296,7 @@ class PokemonActivity : ComponentActivity() {
         }
     }
 }
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun PokemonList(
     list: List<Pokemon>,
@@ -326,12 +309,11 @@ fun PokemonList(
     state: LazyListState,
     isLoading: Boolean) {
     with(sharedTransitionScope) {
-        val interactionSource = remember { MutableInteractionSource() }
         Column(modifier = modifier.fillMaxSize()) {
 
             TextField(
                 value = searchQuery,
-                onValueChange = {},
+                onValueChange = onInputQuery,
                 singleLine = true,
                 shape = RoundedCornerShape(25),
                 colors = TextFieldDefaults.colors().copy(
@@ -427,10 +409,10 @@ fun DetailsScreen(
             modifier = modifier
         ) {
             var animate by remember { mutableStateOf(false) }
-            val offset by animateDpAsState(if (animate) (-30).dp else 0.dp,label = "offset")
+            val offset by animateIntAsState(if (animate) (-80) else 0,label = "offset")
             val height by animateDpAsState(targetValue = if (animate) 0.dp else 30.dp, label = "height")
             LaunchedEffect(true) {
-                animate=true;
+                animate=true
             }
             TextField(
                 value = searchQuery,
@@ -449,7 +431,7 @@ fun DetailsScreen(
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp, vertical = 8.dp)
                     .height(height)
-                    .absoluteOffset(y = offset)
+                    .absoluteOffset { IntOffset(0,offset) }
             )
             Card(
                 modifier = Modifier
