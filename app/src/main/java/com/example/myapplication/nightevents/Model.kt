810cda1ea54f6cde7329e32b8f50dad18e396bbb
fagner ferreira <fagnerrc12@gmail.com>
2024-12-18 14:38:54 -0300

    pokemon app

---
+++ app/src/main/java/com/example/myapplication/nightevents/Model.kt
@@ -0,0 +1,39 @@
+package com.example.myapplication.nightevents
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import com.example.myapplication.R
+
+data class Event(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val date: String,
+    val location: String,
+    val isFavorite: MutableState<Boolean> = mutableStateOf(false),
+    val isSubscribed: MutableState<Boolean> = mutableStateOf(false),
+    val imageRes: Int
+)
+
+val eventList = listOf(
+    Event(
+        id = 1,
+        title = "Conferência de Tecnologia 2024",
+        description = "Tendências em tecnologia.",
+        date = "2024-12-15",
+        location = "Parque Tecnológico",
+        isFavorite = mutableStateOf(false),
+        isSubscribed = mutableStateOf(false),
+        imageRes = R.drawable.icon
+    ),
+    Event(
+        id = 2,
+        title = "Conferência de IA 2024",
+        description = "Tendências em IA.",
+        date = "2024-12-15",
+        location = "Parque Tecnológico",
+        isFavorite = mutableStateOf(false),
+        isSubscribed = mutableStateOf(false),
+        imageRes = R.drawable.img1
+    ),
+)
