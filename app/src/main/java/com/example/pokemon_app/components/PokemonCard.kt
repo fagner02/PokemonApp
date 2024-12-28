aae9b8239a10dfc4be6de24c993b95ea9da9ed88
fagner ferreira <fagnerrc12@gmail.com>
2024-12-28 01:34:33 -0300

    pokemon app

--- app/src/main/java/com/example/pokemon_app/components/PokemonCard.kt
+++ app/src/main/java/com/example/pokemon_app/components/PokemonCard.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -128,6 +129,19 @@ fun PokemonCard(
             }
         }
     }
+}
+
+@Composable
+fun PokemonCardPlaceholder(
+) {
+    Box(
+        modifier = Modifier
+            .animateContentSize()
+            .fillMaxWidth()
+            .padding(8.dp)
+            .height(112.dp)
+            .shimmer()
+    )
 }
 
 @Composable
--- app/src/main/java/com/example/pokemon_app/components/PokemonList.kt
+++ app/src/main/java/com/example/pokemon_app/components/PokemonList.kt
@@ -3,20 +3,23 @@ package com.example.pokemon_app.components
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.stopScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -32,9 +35,12 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import com.example.pokemon_app.api.Pokemon
 import kotlinx.coroutines.delay
@@ -85,7 +91,7 @@ fun PokemonList(
             )
 
             val coroutine = rememberCoroutineScope()
-
+            val filtered = list.filter { it.name.contains(searchQuery) }
             LazyColumn(
                 verticalArrangement = Arrangement.spacedBy(8.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
@@ -95,57 +101,51 @@ fun PokemonList(
                 state = state,
                 userScrollEnabled = !isAnimating
             ) {
-                itemsIndexed(list.filter { it.name.contains(searchQuery) },
-                    key = {_, pokemon -> pokemon.name}) { index, pokemonName ->
-                    var selecting by remember { mutableStateOf(false) }
-                    var timedout by remember { mutableStateOf(false) }
-                    if (selecting && (state.isScrollInProgress && !timedout)) {
-                        DisposableEffect(Unit) {
-                            onDispose {
-                                coroutine.launch {
-                                    state.stopScroll()
-                                }
-                                selecting = false
-                                timedout = false
+                items(filtered.size + if (isLoading) (if (searchQuery=="") 50 else 5) else 0,
+                    key = { if (it < filtered.size) filtered[it].name else "$it" }) {
+                    if (it < filtered.size) {
+                        val pokemon = filtered[it]
+                        var selecting by remember { mutableStateOf(false) }
+                        var timedout by remember { mutableStateOf(false) }
+                        if (selecting && (state.isScrollInProgress && !timedout)) {
+                            DisposableEffect(Unit) {
+                                onDispose {
+                                    coroutine.launch {
+                                        state.stopScroll()
+                                    }
+                                    selecting = false
+                                    timedout = false
 
-                                setIsAnimating()
-                                onSelectPokemon(pokemonName.name)
-                            }
-                        }
-                    }
-                    if (selecting) {
-                        LaunchedEffect(Unit) {
-                            while (true) {
-                                delay(100.milliseconds)
-                                timedout = true
-                            }
-                        }
-                    }
-
-                    PokemonCard(
-                        pokemonName,
-                        index,
-                        scrollToItem = {
-                            if(!isAnimating) {
-                                coroutine.launch {
-                                    state.animateScrollToItem(index = index, -500)
+                                    setIsAnimating()
+                                    onSelectPokemon(pokemon.name)
                                 }
-                                selecting = true
                             }
-                        },
-                        animatedVisibilityScope = animatedVisibilityScope,
-                        sharedTransitionScope = sharedTransitionScope,
-                    )
-                }
-                if (isLoading) {
-                    item {
-                        Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-                            CircularProgressIndicator(
-                                color = MaterialTheme.colorScheme.primary,
-                                strokeCap = StrokeCap.Round,
-                                strokeWidth = 3.dp
-                            )
                         }
+                        if (selecting) {
+                            LaunchedEffect(Unit) {
+                                while (true) {
+                                    delay(100.milliseconds)
+                                    timedout = true
+                                }
+                            }
+                        }
+
+                        PokemonCard(
+                            pokemon,
+                            it,
+                            scrollToItem = {
+                                if (!isAnimating) {
+                                    coroutine.launch {
+                                        state.animateScrollToItem(index = it, -500)
+                                    }
+                                    selecting = true
+                                }
+                            },
+                            animatedVisibilityScope = animatedVisibilityScope,
+                            sharedTransitionScope = sharedTransitionScope,
+                        )
+                    } else {
+                        PokemonCardPlaceholder()
                     }
                 }
             }
@@ -153,4 +153,29 @@ fun PokemonList(
     }
 }
 
+fun Modifier.shimmer(): Modifier = composed {
+    var size by remember { mutableStateOf(IntSize.Zero) }
+    val transition = rememberInfiniteTransition(label = "shimmer")
+
+    val x by transition.animateFloat(
+        0f,
+        size.width.toFloat() * 2,
+        animationSpec = infiniteRepeatable(
+            tween(500, easing = LinearEasing)
+        ), label = "shimmer"
+    )
+
+    background(
+        Brush.horizontalGradient(
+            listOf(
+                MaterialTheme.colorScheme.surfaceContainerHigh,
+                Color(0xff555555),
+                MaterialTheme.colorScheme.surfaceContainerHigh,
+            ),
+            startX = x - size.width.toFloat(),
+            endX = x
+        ),
+        shape = RoundedCornerShape(10.dp)
+    ).onGloballyPositioned { size = it.size }
+}
 
