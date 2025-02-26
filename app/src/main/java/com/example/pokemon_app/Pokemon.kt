3d6dcb95b8cc99c58497bd2142cb38af77e0ba78
fagner ferreira <fagnerrc12@gmail.com>
2025-02-26 12:54:35 -0300

    fix login screen

--- app/src/main/java/com/example/pokemon_app/Pokemon.kt
+++ app/src/main/java/com/example/pokemon_app/Pokemon.kt
@@ -23,11 +23,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
